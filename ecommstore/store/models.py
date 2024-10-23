3d8c369a5ce7cd079f65bafea68e51d1b311730e
Dushyant Baweja <dbaweja@gmail.com>
2024-10-23 18:39:08 +0530

    Specify max_digits in decimal field for Product Model

--- ecommstore/store/models.py
+++ ecommstore/store/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
 
 class Category(models.Model):
