4715bf645ad51c38121a434722ec0c1ca27b25d2
Dushyant Baweja <dbaweja@gmail.com>
2024-10-25 13:01:04 +0530

    Configure to run with pymysql

--- ecommstore/ecommstore/settings.py
+++ ecommstore/ecommstore/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Replace mysqlclient with pymysql to connect to Amazon RDS
+pymysql.install_as_MySQLdb()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
