79a24ffd9132d007d31fdc70551f8beeed7aa54c
Dushyant Baweja <dbaweja@gmail.com>
2024-11-06 23:05:36 +0530

    Add payment gateways abstract classes

---
+++ ecommstore/payment/gateways/payment_gateways.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+import razorpay
+import json
+from ecommstore import settings
+
+class PaymentGateway(ABC):
+    @abstractmethod
+    def generate_payment_link(self, order_id, amount):
+        pass
+
+class RazorpayGateway(PaymentGateway):
+    def __init__(self):
+        self.client = razorpay.Client(auth=(settings.RAZORPAY_API_KEY, settings.RAZORPAY_API_SECRET))
+
+    def generate_payment_link(self, order_id, amount):
+        #   order = self.client.order.create({"amount": amount, "currency": "INR", "payment_capture": "1"})
+        #   return order["short_url"]
+        payment_data = {
+            "amount": amount,
+            "currency": "INR",
+            "description": "For XYZ purpose",
+            "customer": {
+                "name": "karan ",
+                "email": "karan.bhatia_1@scaler.com",
+                "contact": "+918295053001"
+            },
+            "notify": {
+                "sms": True,
+                "email": True
+            },
+            "reminder_enable": True,
+            "callback_url": "https://google.com/",
+            "callback_method": "get"
+        }
+
+        payment_link = self.client.payment_link.create(payment_data)
+        return json.dumps(payment_link)
