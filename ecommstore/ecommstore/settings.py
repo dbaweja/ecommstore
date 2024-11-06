0b7638befd354ba24f3a70556a648d3a531b9d81
Dushyant Baweja <dbaweja@gmail.com>
2024-11-06 22:46:32 +0530

    Create PaymentView and configure urls

--- ecommstore/ecommstore/settings.py
+++ ecommstore/ecommstore/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "store",
     "rest_framework",
+    "payment",
 ]
 
 MIDDLEWARE = [
--- ecommstore/payment/views.py
+++ ecommstore/payment/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+class PaymentView(APIView):
+    def __init__(self):
+        super().__init__()
+
+    def post(self, request):
+        pass
 # Create your views here.
