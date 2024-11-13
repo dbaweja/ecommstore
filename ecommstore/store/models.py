de4d01a9f2e2cc295ad5a871c5042fa05bc51d7b
Dushyant Baweja <dbaweja@gmail.com>
2024-11-13 17:13:35 +0530

    Modify orderitem model to include price not total

--- ecommstore/store/models.py
+++ ecommstore/store/models.py
@@ -16,3 +16,9 @@ class Order(models.Model):
     amount = models.IntegerField()
     payment_status = models.BooleanField(default=False)
     payment_id = models.CharField(max_length=255, null=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
--- ecommstore/store/serializers.py
+++ ecommstore/store/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, Category, Order
+from .models import Product, Category, Order, OrderItem
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +15,8 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = "__all__"
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity', 'price']
